af142c4e9afeac1d390f4019c6517dd2873244a1
farhan0167 <>
2021-03-16 00:52:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './components/Home'
+import SignUp from './components/Signup'
+import {AuthProvider} from './contexts/AuthContext'
+import UserProfile from './components/UserProfile'
+import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+        <Router>
+          <Switch>
+            <Route exact path = "/" component = {Home}/>
+            <AuthProvider>
+              <Route path = "/signup" component = {SignUp}/>
+              <Route path = "/login" component = {Login}/>
+              <PrivateRoute path = "/profile" component = {UserProfile}/>
+            </AuthProvider>
+          </Switch>
+        </Router>
+      </React.Fragment>
+
+    );
+  }
 }
 
 export default App;
